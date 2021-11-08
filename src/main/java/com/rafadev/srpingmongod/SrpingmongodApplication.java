f6069b078370f13219437faf9d07b4e0d2511ea9
rafa9694 <rafa9694@gmail.com>
2021-11-08 11:01:08 -0300

    Project created

---
+++ src/main/java/com/rafadev/srpingmongod/SrpingmongodApplication.java
@@ -0,0 +1,13 @@
+package com.rafadev.srpingmongod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingmongodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingmongodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafadev/srpingmongod/SrpingmongodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafadev.srpingmongod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingmongodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
