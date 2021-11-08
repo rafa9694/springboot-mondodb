2f6a93ecf08d47bb3cb1d6ec91e01083012eaa0f
rafa9694 <rafa9694@gmail.com>
2021-11-08 20:41:21 -0300

    Obtendo um usuário por id

--- src/main/java/com/rafadev/srpingmongod/services/UserService.java
+++ src/main/java/com/rafadev/srpingmongod/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rafadev.srpingmongod.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafadev.srpingmongod.domain.User;
 import com.rafadev.srpingmongod.repository.UserRepository;
+import com.rafadev.srpingmongod.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,16 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+
+		Optional<User> user = repo.findById(id);
+
+		if (user.isEmpty()) {
+			throw new ObjectNotFoundException("Objecto não encontrado");
+		}
+
+		return user.get();
 	}
 }
