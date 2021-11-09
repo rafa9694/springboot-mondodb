798bd95818730e51b6f5959ad93f1a5b147e1436
rafa9694 <rafa9694@gmail.com>
2021-11-08 23:49:55 -0300

    Consulta simples @query

--- src/main/java/com/rafadev/srpingmongod/repository/PostRepository.java
+++ src/main/java/com/rafadev/srpingmongod/repository/PostRepository.java
@@ -4,12 +4,16 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rafadev.srpingmongod.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, Serializable> {
+
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
