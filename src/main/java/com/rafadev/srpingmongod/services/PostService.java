f070c0501c0361daaaa67f49dc81ae50baf7dc4c
rafa9694 <rafa9694@gmail.com>
2021-11-08 22:23:40 -0300

    Obtendo post byid

---
+++ src/main/java/com/rafadev/srpingmongod/services/PostService.java
@@ -0,0 +1,34 @@
+package com.rafadev.srpingmongod.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafadev.srpingmongod.domain.Post;
+import com.rafadev.srpingmongod.repository.PostRepository;
+import com.rafadev.srpingmongod.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+
+		Optional<Post> post = repo.findById(id);
+
+		if (post.isEmpty()) {
+			throw new ObjectNotFoundException("Objecto não encontrado");
+		}
+
+		return post.get();
+	}
+
+}
